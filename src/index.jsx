5bc2894eb03db6d0c5821bf3ba0132f5c8b7b485
Lei <llei@che34413m.active.local>
2023-03-15 20:15:28 +0800

    Linting jsx files

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+var a = 1;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
