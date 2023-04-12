75b4d4368bc69235158db27bdbe5a86087cf1fde
leoleoleolei <leo.lei@globalpay.com>
2023-04-12 13:49:28 +0800

    Test CR-bot

--- src/app.jsx
+++ src/app.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 var a = '1234';
 
 function App() {
+  const b = null;
   return (
     <div className="App">
       <header className="App-header">
+        {console.log(b.a)}
         <div>Only test</div>
       </header>
     </div>
